918516ce453285f63c0c410f593da10a47b96319
yourGitHubUsername <Anna@Annas-MBP.fios-router.home>
2020-06-07 18:07:44 -0400

    Changes a little

--- R/makeMovie.R
+++ R/makeMovie.R
@@ -2,16 +2,47 @@
 #'
 #' This function loads ImageJ roi zip files.
 #' It returns a list object with corrected cell IDs if needed.
-#' @param folder .zip folder containg .roi objects
-#' @param full.names a logical value.  If TRUE, the directory path is prepended to the file names to give a relative file path. If FALSE, the file names (rather than paths) are returned.Defaul TRUE.
+#' @param timesSeries 
 #' @return a list object of rois with corrected IDs adjusted to previous time stack, if needed.
 #' @export
 #' @examples
 #' folder<-system.file('data/my.zipfiles', package='yeast')
 #' yeastMovie<-trackCell(folder)
 
+#use the example file daniel made "Untitled1". Use the way we plot in the clustering function to save
+# as png files. Then use ffmpeg on that folder. 
 
 makeMovie <- function(timesSeries){
   
+  newfolder<-'mymovie'
+  dir.create(newfolder)
+  setwd(newfolder)
+  
+  for (i in (timesSeries[[i]])){
+    
+xycoord<-list()
+
+    
+    xAvg<-numeric()
+    yAvg<-numeric()
+    for(l in which(clusters == k)){
+      x <- dimred[l,1:100]
+      y <- dimred[l,101:200]
+      polygon(x , y, border=rgb(0,0,0, 0.1) )
+      xAvg<-cbind(xAvg, x)
+      yAvg<-cbind(yAvg, y)
+    }
+    
+  }
+  
+  for(q in 1:nrow(data)){
+    filename<-paste0('pic', formatC(q, digits = 3, flag='0'), '.png')
+    png(filename = filename, width=1920, height=1080)
+    plot(c(-1, 1), c(-1, 1), type = "n", asp=1, axes=F, xlab='', ylab='')
+    points(data[q,], pch=16, cex=3)
+    dev.off()
+  }
+  
+  system('ffmpeg -r 24 -f image2 -s 1920x1080 -i ./pic%04d.png -vcodec libx264 -pix_fmt yuv420p ../output.mp4')
   
 }
